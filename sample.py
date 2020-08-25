0e600c65ef2ceb9935ee0215e6baa41a1f68099c
mmaithani <mohitmaithani81@gmail.com>
2020-08-25 10:42:27 +0530

    update bot ui and code

--- sample.py
+++ sample.py
@@ -1,13 +1,16 @@
 from flask import Flask,render_template,request
-from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
 
 app = Flask(__name__) 
-english_bot = ChatBot("Chatterbot",storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
-trainer.train("data/data.yml")
-
 
 @app.route("/")
 def index():
@@ -16,10 +19,11 @@ def index():
 @app.route("/get")
 def get_bot_response():
      userText = request.args.get("msg") #get data from input,we write js  to index.html
-     return str(english_bot.get_response(userText))
-
+     # return str(userText)
+     return str(userText)
 
 if __name__ == "__main__":
      app.run(debug = True)
+
 
 
